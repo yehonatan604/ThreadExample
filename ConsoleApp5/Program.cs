540f710e922ae87343ed7bde6f0139f2938bebc2
yehonatan604 <97249220+yehonatan604@users.noreply.github.com>
2022-08-31 22:15:09 +0300

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread1 = new(MyFunc);
+            thread1.Start();
+            MyFunc2();
+            
+        }
+
+        public static void MyFunc()
+        {
+            Console.WriteLine("i am 1");
+            Thread.Sleep(1000);
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public static void MyFunc2()
+        {
+            Console.WriteLine("i am 2");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
